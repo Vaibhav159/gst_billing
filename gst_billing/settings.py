7000201a86414172fc861e35cf6d10b046ec5f16
Vaibhav Lodha <vlodha98@gmail.com>
2025-04-06 21:52:37 +0530

    allowing web apis

--- gst_billing/settings.py
+++ gst_billing/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = local.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.68.111", "*"]
 
 # Application definition
 
