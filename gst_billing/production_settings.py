283134b7e102126d1d99593b2b5a7c0637e52efc
Vaibhav Lodha <vlodha98@gmail.com>
2026-05-07 01:35:06 +0530

    micro changes

--- gst_billing/production_settings.py
+++ gst_billing/production_settings.py
@@ -85,6 +85,7 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
         "HOST": os.environ.get("DB_HOST", "db"),
         "PORT": os.environ.get("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 60,  # Keep database connections alive for 60 seconds
     }
 }
 
